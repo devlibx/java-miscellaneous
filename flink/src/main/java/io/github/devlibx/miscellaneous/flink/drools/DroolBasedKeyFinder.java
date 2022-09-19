e8828625fa7f063b9be5aa06d4c77b9c0ef39244
Harish Bohara <harish.bohara@gmail.com>
2022-09-20 01:11:56 +0530

    [*] added support to pass config in key finder

--- flink/src/main/java/io/github/devlibx/miscellaneous/flink/drools/DroolBasedKeyFinder.java
+++ flink/src/main/java/io/github/devlibx/miscellaneous/flink/drools/DroolBasedKeyFinder.java
@@ -2,20 +2,31 @@ package io.github.devlibx.miscellaneous.flink.drools;
 
 import io.gitbub.devlibx.easy.helper.map.StringObjectMap;
 import io.github.devlibx.easy.flink.utils.KafkaSourceHelper;
+import io.github.devlibx.easy.flink.utils.v2.config.Configuration;
 import io.github.devlibx.easy.rule.drools.ResultMap;
 
 import java.io.Serializable;
 
 public class DroolBasedKeyFinder implements KafkaSourceHelper.ObjectToKeyConvertor<StringObjectMap>, Serializable {
     private final IRuleEngineProvider ruleEngineProvider;
+    private final Configuration configuration;
 
     public DroolBasedKeyFinder(IRuleEngineProvider ruleEngineProvider) {
+        this(ruleEngineProvider, null);
+    }
+
+    public DroolBasedKeyFinder(IRuleEngineProvider ruleEngineProvider, Configuration configuration) {
         this.ruleEngineProvider = ruleEngineProvider;
+        this.configuration = configuration;
     }
 
     private String keyAsString(StringObjectMap value) {
         ResultMap result = new ResultMap();
-        ruleEngineProvider.getDroolsHelper().execute("expiry-event-trigger-partition-key", value, result);
+        if (configuration != null) {
+            ruleEngineProvider.getDroolsHelper().execute("expiry-event-trigger-partition-key", value, result, configuration);
+        } else {
+            ruleEngineProvider.getDroolsHelper().execute("expiry-event-trigger-partition-key", value, result);
+        }
         return result.getString("partition-key", "");
     }
 
